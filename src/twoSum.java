ee992d7da8ee7d40badee93f0abb12655b0d06f9
deckofmanystrings <kingsadabobby@gmail.com>
2021-04-16 13:53:51 -0700

    Initial commit

---
+++ src/twoSum.java
@@ -0,0 +1,24 @@
+/*
+Given an array of numbers and a target value, return the indices of two numbers that add up to the target value.
+You can assume there is always only one correct answer in the given array.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+*/
+
+public class twoSum {
+    public int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] + nums[i] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
